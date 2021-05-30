c0b02027e58fa97331ee551b74bf042d3f7ee24d
alpsilva <alps2@cin.ufpe.br>
2021-05-30 00:15:39 -0300

    test and good practices

---
+++ projeto0/functions/produto_escalar.py
@@ -0,0 +1,2 @@
+def produtoEscalar(vetor1, vetor2):
+    return (vetor1.x1 * vetor2.x1) + (vetor1.x2 * vetor2.x2) + (vetor1.x3 * vetor2.x3)
---
+++ projeto0/test.py
@@ -0,0 +1,7 @@
+from functions import *
+from structures.Vetor import Vetor
+
+v1 = Vetor(2, 3, 2)
+v2 = Vetor(3, 2, 3)
+n = produtoEscalar.produtoEscalar(v1, v2)
+print(n)
